3ec8846892dbdf56193a5a3ea089764df29a837e
jasonwynn10 <jasonwynn10@gmail.com>
2017-05-02 14:25:08 -0400

    Create MurderListener.php

---
+++ src/jasonwynn10/murder/MurderListener.php
@@ -0,0 +1,23 @@
+<?php
+namespace jasonwynn10\murder;
+
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+
+class MurderListener impliments Listener {
+	private $plugin;
+	public function __construct(Main $plugin) {
+		$this->plugin = $plugin;
+	}
+	public function onDamage(EntityDamageEvent $ev) {
+		if($ev instanceof EntityDamageByEntityEvent) {
+			//
+		}
+		/** @var Player $player */
+		if(($player = $ev->getEntity()) instanceof Player and $this->plugin->inSession($player)) {
+			$player->kill(); //kill the player in one hit if they are in the session and attacked by the murderer
+		}
+	}
+	//TODO cancel item drops
+}
