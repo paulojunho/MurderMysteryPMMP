396b48ae275dc4555ba39576cc3b86e130da9097
jasonwynn10 <jasonwynn10@gmail.com>
2017-05-02 13:33:44 -0400

    Create MurderMystery.php

---
+++ src/jasonwynn10/murder/MurderMystery.php
@@ -0,0 +1,18 @@
+<?php
+namespace jasonwynn10\murder;
+
+use use pocketmine\command\PluginCommand;
+
+class MurderMystery extends PluginCommand {
+	public function __construct(Main $plugin) {
+		parent::__construct("murdermystery", $plugin);
+		$this->setPerission("murder.mystery.play");
+		$this->setDescription("Adds the player to the Murder Mystery Game Queue");
+		$this->setAliases(["mm", "murder"]);
+	}
+	public function execute(CommandSender $sender, $alias, array $args) {
+		if($sender instanceof Player and $this->testPermission($sender)) {
+			//TODO check args
+		}
+	}
+}
