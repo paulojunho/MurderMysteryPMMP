7987e0b3c8c3fde1429e5528c5876f1f5bbd6954
jasonwynn10 <jasonwynn10@gmail.com>
2017-05-03 16:24:59 -0400

    Added Multi-lang API

--- src/jasonwynn10/murder/Main.php
+++ src/jasonwynn10/murder/Main.php
@@ -1,19 +1,24 @@
 <?php
 namespace jasonwynn10\murder;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\lang\BaseLang;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\utils\TextFormat as TF;
 use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
 
 class Main extends PluginBase {
+	/** @var BaseLang $baseLang */
+	private $baseLang = null;
 	public function onLoad() {
 		$this->getCommandMap()->register(MurderMystery::class, new MurderMystery($this));
 		$this->getServer()->getPluginManager()->registerEvents(new MurderListener($this), $this);
 	}
 	public function onEnable() {
 		$this->saveDefaultConfig();
+		$lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANGUAGE);
+        	$this->baseLang = new BaseLang($lang, $this->getFile() . "resources/");
 		$this->getLogger()->notice(TF::GREEN."Enabled!");
 	}
 	public function onDisable() {
@@ -21,6 +26,14 @@ class Main extends PluginBase {
 	}
 	
 	// API
+	
+	/**
+	 * @api
+	 * @return BaseLang
+	 */
+	public function getLanguage() : BaseLang {
+		return $this->baseLang;
+	}
 	
 	public function addQueue(Player $player) {
 		//TODO
