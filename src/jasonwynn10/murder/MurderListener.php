924e14e4d8ea04d57e2fc5f573731d890fe12bce
jasonwynn10 <jasonwynn10@gmail.com>
2017-05-22 20:38:44 -0400

    cancelled item drops if in session

--- src/jasonwynn10/murder/MurderListener.php
+++ src/jasonwynn10/murder/MurderListener.php
@@ -3,6 +3,7 @@ namespace jasonwynn10\murder;
 
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\Listener;
 
 class MurderListener implements Listener {
@@ -12,12 +13,17 @@ class MurderListener implements Listener {
 	}
 	public function onDamage(EntityDamageEvent $ev) {
 		if($ev instanceof EntityDamageByEntityEvent) {
-			//
-		}
-		/** @var Player $player */
-		if(($player = $ev->getEntity()) instanceof Player and $this->plugin->inSession($player)) {
-			$player->kill(); //kill the player in one hit if they are in the session and attacked by the murderer
+			/** @var Player $player */
+			if(($player = $ev->getEntity()) instanceof Player and $this->plugin->inSession($player) != false) {
+				$player->kill(); //kill the player in one hit if they are in the session and attacked by the murderer
+			}
 		}
 	}
-	//TODO cancel item drops
+	public function onDeath(PlayerDeathEvent $ev) {
+		if(($session = $this->plugin->inSession($event->getPlayer())) != false and $session->getRole($ev->getPlayer()) == "detective") {
+			$event->setDrops([Item::get(Item::BOW), Item::get(Item::ARROW)]); // only drop bow if in session and a detective
+		}elseif(($session = $this->plugin->inSession($event->getPlayer())) != false) {
+			$event->setDrops([]); // no drops if in session and not a detective
+		}
+	}
 }
